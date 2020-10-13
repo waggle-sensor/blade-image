42ed95499be7ea5d9a4a29ccd8c4fe801cfea6eb
Joe Swantek <joseph.swantek@northwestern.edu>
2020-10-13 11:41:28 -0500

    SAGE-517: finalize initial Dell image
    - Pre-download all Debian packages and include in ISO
    - Populate ROOTFS folder with SAGE additions to final file system
    - Updated partition layout (16GB root, 256GB sys-data, * plugin-data)
    - Use a single ./build.sh script
    - Minimize post installation steps / manual intervention

--- build_version.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-BASE_VERSION="$(cat 'version' | xargs).${BUILD_NUMBER:-local}"
-GIT_SHA=$(git rev-parse --short HEAD)
-DIRTY=$([[ -z $(git status -s) ]] || echo '-dirty')
-PROJ_VERSION=${BASE_VERSION}-${GIT_SHA}${DIRTY}
-
-echo $PROJ_VERSION > blade-image-build/ROOTFS/opt/sage/blade/image/wscripts/version
