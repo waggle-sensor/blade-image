616ee4364217773039a78f9e3a02b235c59df933
Joe Swantek <joseph.swantek@northwestern.edu>
2022-02-16 20:45:37 -0600

    SAGE-691: mount the media partition during post installation to setup docker/k3s

--- ROOTFS/etc/waggle/postinst/postinst.sh
+++ ROOTFS/etc/waggle/postinst/postinst.sh
@@ -2,6 +2,9 @@
 
 # Operations to perform at the very end of ISO installation on the target system
 
+MEDIA_PARTITION=/dev/sda5
+MEDIA_PATH=/media/plugin-data
+
 # Remove undesired packages
 echo "Remove undesired packages"
 apt-get purge -y \
@@ -17,19 +20,22 @@ echo "PasswordAuthentication no" >> /etc/ssh/sshd_config
 echo "PermitRootLogin without-password" >> /etc/ssh/sshd_config
 echo "ListenAddress 127.0.0.1" >> /etc/ssh/sshd_config
 
+echo "Mount ${MEDIA_PARTITION} -> ${MEDIA_PATH}"
+mount ${MEDIA_PARTITION} ${MEDIA_PATH}
+
 # configure docker plugin-data partition
 echo "Move Docker storage to external media"
 systemctl stop docker
 rm -rf /var/lib/docker
-mkdir -p /media/plugin-data/docker
-ln -s /media/plugin-data/docker /var/lib/docker
+mkdir -p ${MEDIA_PATH}/docker
+ln -s ${MEDIA_PATH}/docker /var/lib/docker
 
 #configure k3s plugin-data partition
 echo "Move k3s storage to external media"
-mkdir -p /media/plugin-data/k3s/etc/rancher
-mkdir -p /media/plugin-data/k3s/kubelet
-mkdir -p /media/plugin-data/k3s/rancher
-ln -s /media/plugin-data/k3s/etc/rancher/ /etc/rancher
-ln -s /media/plugin-data/k3s/kubelet/ /var/lib/kubelet
-ln -s /media/plugin-data/k3s/rancher/ /var/lib/rancher
+mkdir -p ${MEDIA_PATH}/k3s/etc/rancher
+mkdir -p ${MEDIA_PATH}/k3s/kubelet
+mkdir -p ${MEDIA_PATH}/k3s/rancher
+ln -s ${MEDIA_PATH}/k3s/etc/rancher/ /etc/rancher
+ln -s ${MEDIA_PATH}/k3s/kubelet/ /var/lib/kubelet
+ln -s ${MEDIA_PATH}/k3s/rancher/ /var/lib/rancher
 INSTALL_K3S_SKIP_DOWNLOAD=true INSTALL_K3S_SKIP_START=true /etc/waggle/k3s_config/k3s_install.sh
