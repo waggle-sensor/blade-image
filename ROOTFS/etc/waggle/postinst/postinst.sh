b91224838a6f8f33767913bc780da194f829f60d
Joe Swantek <joseph.swantek@northwestern.edu>
2022-02-16 20:58:43 -0600

    SAGE-691: configure the overlayroot file system in the post install script

--- ROOTFS/etc/waggle/postinst/postinst.sh
+++ ROOTFS/etc/waggle/postinst/postinst.sh
@@ -19,6 +19,10 @@ echo "Removing efi and media/system-data from /etc/fstab"
 sed -i '/efi/d' /etc/fstab
 sed -i '/\/media\/system-data/d' /etc/fstab
 
+# configure root overlay
+echo "Enable overlayroot file system"
+sed -i 's|overlayroot=""|overlayroot="device:dev=/dev/sda4,timeout=180,recurse=0,swap=1"|' /etc/overlayroot.conf
+
 # update ssh conf
 echo "Configure SSHD Config"
 echo "PasswordAuthentication no" >> /etc/ssh/sshd_config
