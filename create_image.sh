f8246fa4568d81b04d1b9d30bde328cc78ebd088
Joe Swantek <joseph.swantek@northwestern.edu>
2022-02-17 09:53:54 -0600

    SAGE-691: change VERSION variable name to PROJ_VERSION

--- create_image.sh
+++ create_image.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-echo "Building Dell Waggle Ubuntu [$VERSION]"
+echo "Building Dell Waggle Ubuntu [$PROJ_VERSION]"
 
 #grab neccessary build files
 cp /iso_tools/preseed.seed /iso/preseed/
@@ -13,7 +13,7 @@ ls -lah /iso/pool/contrib/*
 echo "Additional Debs: $(ls -la /iso/pool/contrib/* | wc -l) [$(du -hs /iso/pool/contrib/)]"
 
 mkdir -p /ROOTFS/etc
-echo $VERSION > /ROOTFS/etc/waggle_version_os
+echo $PROJ_VERSION > /ROOTFS/etc/waggle_version_os
 
 echo "Downloading & Installing K3S"
 mkdir -p /ROOTFS/usr/local/bin/
@@ -26,7 +26,7 @@ echo "Copy SAGE rootfs to ISO"
 cp -r /ROOTFS /iso/
 
 echo "Make the ISO"
-ouputfile="${OUTPUT_NAME}_${VERSION}.iso"
+ouputfile="${OUTPUT_NAME}_${PROJ_VERSION}.iso"
 pushd /iso
 mkisofs -D -r -V "AUTOINSTALL" -cache-inodes -J -l -b isolinux/isolinux.bin \
     -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -o \
