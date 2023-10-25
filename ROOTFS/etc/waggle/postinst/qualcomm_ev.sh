b03bdb2d2689520040f09f254148e73f4f6433ee
hpeliuhan <han.liu@hpe.com>
2023-10-25 23:07:41 +0000

    Required enviornmental variables to load the library.

---
+++ ROOTFS/etc/waggle/postinst/qualcomm_ev.sh
@@ -0,0 +1,9 @@
+LD_LIBRARY_PATH=“$LD_LIBRARY_PATH:/opt/qti-aic/dev/lib/x86_64” 
+PATH="/usr/local/bin:$PATH" 
+PATH="$PATH:/opt/qti-aic/tools:/opt/qti-aic/exec:/opt/qti-aic/scripts" 
+QRAN_EXAMPLES="/opt/qti-aic/examples" 
+PYTHONPATH="$PYTHONPATH:/opt/qti-aic/dev/lib/x86_64" 
+QAIC_APPS="/opt/qti-aic/examples/apps" 
+QAIC_LIB="/opt/qti-aic/dev/lib/x86_64/libQAic.so" 
+QAIC_COMPILER_LIB="/opt/qti-aic/dev/lib/x86_64/libQAicCompiler.so" 
+
